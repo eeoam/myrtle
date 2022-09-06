54fbf8f71ed5b255541235b2dbf6beb2ad39c099
eeoam <eric@emurgo.io>
2022-09-06 08:35:06 +0000

    Added sign and submit functions.

--- src/Myrtle/Commands.hs
+++ src/Myrtle/Commands.hs
@@ -54,19 +54,7 @@ sending = Sending
     , out_file = "tx1.draft"
     }
 
-sraw = sending
-    & #balance .~ 490425078
-    & #amt .~ 4000000
-    & #tx_ins .~ [ "1fb637d1bfcfc792cfbf9cb869249789e9aff4e7636f3531f9de449f25d180cf#0" ]
-    & #tx_outs .~ [ receiver ++ "+0", sender ++ "+0"]
 
-
-sr = sraw
-    & #fee .~ 174433
-    & #tx_outs .~ [ receiver ++ "+" ++ (show $ sr ^. #amt), sender ++ "+" ++ (show change)]
-    & #current_slot .~ 68036764 -- 2022.09.5:20:30
-    & #secs_valid .~ 16 * 60
-    where change = sr ^. #balance - (sr ^. #amt + sr ^. #fee)
 
 
 mkTxIn, mkTxOut :: String -> String
@@ -103,10 +91,7 @@ costing = Costing
     , witness_count = 0
     } 
 
-cargz = costing
-    & #tx_in_count .~ 1
-    & #tx_out_count .~ 2
-    & #witness_count .~ 1
+
 
 cost :: Costing -> IO ExitCode
 cost = runProcess . shell . costCmd
@@ -122,6 +107,25 @@ costCmd r =
     --witness-count #{r ^. #witness_count}
     |]
 
+-- | Signing and submitting a transaction.
+sign :: FilePath -> FilePath -> FilePath -> IO ExitCode
+sign skfile unsigned signed = (runProcess . shell) 
+    [iii|#{cardano_cli} transaction sign
+    #{testnet}
+    --tx-body-file #{unsigned}
+    --signing-key-file #{skfile}
+    --out-file #{signed}
+    |]
+
+
+
+submit :: FilePath -> IO ExitCode
+submit tx = runProcess $ shell
+    [iii|#{cardano_cli} transaction submit
+    #{testnet}
+    --tx-file #{tx}
+    |]
+
 --------------------------------------------------------------
 -- | sender is "/config/workspace/MyPrograms/swap-test/addr/seller.addr"
 sender = "addr_test1vpra4fdvstljtkayvw069tq98q8nnly777d72vghnaxdr2qv068g5"
@@ -129,4 +133,34 @@ sender_key = "/config/workspace/MyPrograms/swap-test/keys/seller.skey"
 
 -- | receiver = "/config/workspace/MyPrograms/swap-test/addr/buyer.addr"
 receiver = "addr_test1vpvwy3sadnsetnzygwlts2z6zx9qx8gpfffsdfr4gm4vnxscwv35d"
+
+-- | *Myrtle.Commands> query sender
+
+sraw = sending
+    & #balance .~ 490425078
+    & #amt .~ 4000000
+    & #tx_ins .~ [ "1fb637d1bfcfc792cfbf9cb869249789e9aff4e7636f3531f9de449f25d180cf#0" ]
+    & #tx_outs .~ [ receiver ++ "+0", sender ++ "+0"]
+
+-- | *Myrtle.Commands> send sraw
+
+cargz = costing
+    & #tx_in_count .~ 1
+    & #tx_out_count .~ 2
+    & #witness_count .~ 1
+
+-- | *Myrtle.Commands> cost cargz
+
+-- | *Myrtle.Commands> query_testnet
+
+sr = sraw
+    & #fee .~ 174433
+    & #tx_outs .~ [ receiver ++ "+" ++ (show $ sr ^. #amt), sender ++ "+" ++ (show change)]
+    & #current_slot .~ 68082963 -- ^ 2022-09-06 0917h
+    & #secs_valid .~ 16 * 60
+    & #out_file .~ "tx1.raw"
+    where change = sr ^. #balance - (sr ^. #amt + sr ^. #fee)
+
+-- | *Myrtle.Commands> sign sender_key (sr ^. #out_file) "tx1.signed"
+-- | *Myrtle.Commands> submit "tx1.signed"
 
